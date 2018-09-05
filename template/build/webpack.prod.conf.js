92bdf964a4046cbbf513e7b787ea3652e9888a3c
hamger <2313303800@qq.com>
2018-09-05 14:40:38 +0800

    fix prod

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -1,15 +1,13 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const utils = require('./utils')
 const resolve = utils.resolve
 
 var webpackConfig = {
-  entry: utils.getEntry('demo/**/index.js', /^(util)$/),
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: '[name]/[name].js',
-    publicPath: '../'
+    filename: 'cvs.js',
+    path: path.resolve(__dirname, '../dist')
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
@@ -48,19 +46,5 @@ var webpackConfig = {
     new UglifyJsPlugin()
   ]
 }
-
-// 在不同的页面中插入对应的js文件
-var htmls = utils.getEntry('demo/**/index.html')
-var pages = Object.keys(htmls)
-pages.forEach(filename => {
-  webpackConfig.plugins.push(
-    new HtmlWebpackPlugin({
-      filename: `${filename}/${filename}.html`,
-      template: htmls[filename],
-      inject: true,
-      chunks: [filename]
-    })
-  )
-})
 
 module.exports = webpackConfig
